ad0bde35e04c9ae9b2f0ab7d8b303b71992e92ab
Biswas Sampad <biswassampads@gmail.com>
2019-04-26 11:58:13 +0530

    sidebar done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+ 
 ];
 
 @NgModule({
